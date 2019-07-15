8a5f3793d617b5a3ef47963018bf3fe787999753
H1ddenGames <skarim3000@gmail.com>
2019-07-15 14:34:22 -0700

    Added testing for all inputs
    The argument testing has been implemented and completed.
    Updated version to 0.2a
    
    Signed-off-by: H1ddenGames <skarim3000@gmail.com>

--- youtube-dl-aria2c-java/src/Downloader.java
+++ youtube-dl-aria2c-java/src/Downloader.java
@@ -1,7 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
 /**
  * This script was created by Shahid Karim on 7/15/19.
  */
 public class Downloader {
+    private static String scriptVersion = "0.2a";
     private static String downloadFile = "";
     private static String downloadLocation = "";
     private static int numberOfThreads = 1;
@@ -17,17 +23,58 @@ public class Downloader {
         System.out.println("\nExample usage: java Downloader.java C:/DL.txt" + " " + "C:/Video Downloads" + " 10");
     }
 
-    public static void main(String[] args) {
-        if(args.length == 0) {
+    private static void checkArguments(String[] args) {
+        if(args.length > 0) {
+            // Options - The usual command line options such as -h, --help for help and -v, --version for version.
+            if (args[0].contentEquals("-h") || args[0].contentEquals("--help")) {
+                System.out.println("\nThis script is a command line wrapper written in Java that uses youtube-dl and aria2c " +
+                        "to download videos when given a file that contains URLs. The required command line arguments are as follows: \n");
+                printRequiredArguments();
+            } else if (args[0].contentEquals("-v") || args[0].contentEquals("--version")) {
+                System.out.println("Version " + scriptVersion);
+            }
+        }
+
+        // Edge cases where there are either no inputs (too few arguments) or 4 or more inputs (too many arguments)
+        if(args.length <= 0) {
             System.out.println("Not enough arguments. This script requires the following: \n");
             printRequiredArguments();
         } else if (args.length > 3) {
             System.out.println("Too many arguments.\n");
             printRequiredArguments();
-        } else if (args[0].contentEquals("-h")) {
-            System.out.println("\nThis script is a command line wrapper written in Java that uses youtube-dl and aria2c " +
-                    "to download videos when given a file that contains URLs. The required command line arguments are as follows: \n");
+        }
+
+        // Normal case where there is more than 0 inputs but less than the 3 required inputs:
+        if(args.length > 0 && args.length < 3) {
+            System.out.println("Not enough arguments. This script requires the following: \n");
             printRequiredArguments();
         }
+
+        // Normal case where there is exactly 3 arguments.
+        if (args.length == 3) {
+            downloadFile = args[0];
+            downloadLocation = args[1];
+            try {
+                numberOfThreads = Integer.parseInt(args[2]);
+            } catch (NumberFormatException e) {
+                System.out.println("An integer is required as the third argument: numberOfThreads." + " Defaulting to 1 thread.");
+            }
+
+            // Do not allow the user to set numberOfThreads higher than the threads available on their CPU.
+            if(numberOfThreads > Runtime.getRuntime().availableProcessors()) {
+                numberOfThreads = Runtime.getRuntime().availableProcessors();
+                System.out.println("The numberOfThreads specified was above the number of threads your CPU supports. " +
+                        "Defaulting to the max amount possible on your CPU: " + numberOfThreads);
+            }
+
+            System.out.println("Starting the download script with the following inputs: ");
+            System.out.println(downloadFile);
+            System.out.println(downloadLocation);
+            System.out.println(numberOfThreads);
+        }
+    }
+
+    public static void main(String[] args) {
+        checkArguments(args);
     }
 }
